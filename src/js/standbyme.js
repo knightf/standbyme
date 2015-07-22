08b00ae668fdbcff5d79cd674c82237a9cd8bec9
Eric Han <knightf86@gmail.com>
2015-07-22 14:48:53 -0400

    element can now stick to the bottom

--- src/js/standbyme.js
+++ src/js/standbyme.js
@@ -7,10 +7,14 @@
 
 			e.id = i++;
 			e.ele = element;
+			e.wrapper = null;
+
 			e.offset = e.getSize(e.ele);
-			e.wrapperFlag = false;
 			e.parentEle = e.ele.parentNode;
 			e.parentOffset = e.getSize(e.parentEle);
+
+			e.wrapperFlag = false;
+			e.bottomFlag = false;
 
 			e.init();
 		}
@@ -22,27 +26,43 @@
 	StandByMe.prototype.init = function(){
 		var e = this;
 		//bind events
-		$(window).on('scroll.standbyme.id' + e.id, {ele : e}, e.onScroll); //listen to scroll
-		$(window).on('resize.standbyme.id' + e.id, {ele : e}, e.onResize); //listen to resize
+		$(window).on('scroll.standbyme.id-' + e.id, {ele : e}, e.onScroll); //listen to scroll
+		$(window).on('resize.standbyme.id-' + e.id, {ele : e}, e.onResize); //listen to resize
 	}
 
 	StandByMe.prototype.onScroll = function(event){
 		var ele = event.data.ele;
 
-		//check if the element is out of the boundary
-		if(((window.scrollY - ele.offset.top) > 0) && ((window.scrollY + parseFloat(ele.getSize().height) < ele.parentOffset.bottom))){
-			//off the screen
+		//check if the element is out of the viewport
+		if((window.scrollY - ele.offset.top) > 0){
+			//off the viewport
 			if(!ele.wrapperFlag){
 				//if it is not wrapped, wrap it
 				ele.wrap();
 			}
 		}else{
-			//not off the screen
+			//in the view port
 			if(ele.wrapperFlag){
 				//if it is wrapped, unwrap it
 				ele.unwrap();
 			}
 		}
+
+		//check if the element should be at the bottom of its parent
+		if((window.scrollY + ele.offset.height) > ele.parentOffset.bottom){
+			//should be
+			if(!ele.bottomFlag && ele.wrapperFlag){
+				//not at bottom, let it stay
+				ele.stayBottom();
+			}
+		}else{
+			//should not be
+			if(ele.bottomFlag && ele.wrapperFlag){
+				//at bottom, release it
+				ele.releaseBottom();
+			}
+		}
+				
 	}
 
 	StandByMe.prototype.onResize = function(event){
@@ -53,7 +73,7 @@
 		ele.parentOffset = ele.getSize(ele.parentEle);
 
 		//adjust the wrapper
-		var wrapper = document.getElementById('standbymeWrapper-' + ele.id);
+		var wrapper = ele.wrapper;
 		if(wrapper){
 			var style = ele.ele.currentStyle || window.getComputedStyle(ele.ele),
 				marginLeft = parseFloat(style.marginLeft);
@@ -101,14 +121,10 @@
 		wrapper.className = 'standbyme-wrapper';
 		wrapper.id = 'standbymeWrapper-' + e.id;
 		//assign the right size
-		//assign width and height
-		var size = e.getSize();
-		wrapper.width = size.width;
-		wrapper.height = size.height;
 		//assign left, minus the margin-left
 		var style = e.ele.currentStyle || window.getComputedStyle(e.ele),
 			marginLeft = parseFloat(style.marginLeft);
-		wrapper.style.left = e.offset.left - marginLeft;
+		wrapper.style.left = e.offset.left - marginLeft + 'px';
 
 		return wrapper;
 	}
@@ -122,6 +138,7 @@
 
 		//add the patch class to the element
 		e.ele.className += ' standbyme-hidden';
+		e.wrapper = wrapper;
 		document.body.appendChild(wrapper);
 
 		e.wrapperFlag = true;
@@ -129,13 +146,36 @@
 
 	StandByMe.prototype.unwrap = function(){
 		var e = this;
-			wrapper = document.getElementById('standbymeWrapper-' + e.id);
+			wrapper = e.wrapper;
 		
 		//remove the patch class on the element
 		e.ele.className = e.ele.className.replace(' standbyme-hidden', '');
 		document.body.removeChild(wrapper);
 
+		e.wrapper = null;
 		e.wrapperFlag = false;
+	}
+
+	StandByMe.prototype.stayBottom = function(){
+		var e = this,
+			wrapper = e.wrapper;
+
+		//set wrapper's correct top value
+		wrapper.className = 'standbyme-wrapper-bottom';
+		wrapper.style.top = e.parentOffset.bottom - e.offset.height + 'px';
+
+		e.bottomFlag = true;
+	}
+
+	StandByMe.prototype.releaseBottom = function(){
+		var e = this,
+			wrapper = e.wrapper;
+
+		//set wrapper's top value 0
+		wrapper.className = 'standbyme-wrapper';
+		wrapper.style.top = 0;
+
+		e.bottomFlag = false;
 	}
 
 	$.fn.standbyme = function(){
